827ed84bd1b5aebe25f0c6e7fb34f2aa89450f62
Evan Muhammad <evanmuhammad107@gmail.com>
2026-08-13 22:06:00 +0300

    Set up Spring Boot backend project

---
+++ backend/src/test/java/com/vellora/vellora_backend/VelloraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vellora.vellora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelloraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
